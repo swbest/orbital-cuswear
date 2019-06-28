a9a953d0b3623c93418d65ca2996eae93d23764f
Gary <garylimky@hotmail.com>
2019-06-28 19:05:31 +0800

    Gary's version of app

---
+++ Assets/ModelsManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelsManager : MonoBehaviour
+{
+    public GameObject[] Models;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Disable All models at start
+        DisableAll();
+        
+    }
+
+    void DisableAll()
+    {
+        foreach (var item in Models)
+        {
+            item.SetActive(false);
+        }
+    }
+
+    void SetModel(int index)
+    {
+        //Disable all other models before enabling a model
+        DisableAll();
+        Models[index].SetActive(true);
+    }
+  
+  
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject ModelsManager;
+    public Sprite[] SelectedSprites;
+    public Sprite[] NormalSprites;
+    public Image[] SliderImages;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        DeselectAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectModel(int index)
+    {
+        ModelsManager.SendMessage("SetModel",index);
+        DeselectAll();
+        SliderImages[index].sprite = SelectedSprites[index];
+    }
+
+    void DeselectAll()
+    {
+        for ( int i = 0; i < SliderImages.Length; i++)
+        {
+            SliderImages[i].sprite = NormalSprites[i];
+        }
+    }
+}
