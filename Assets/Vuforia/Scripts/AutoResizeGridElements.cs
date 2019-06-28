09f034222dffe1d5b42dc049f4d0920e78de0a82
Gary <garylimky@hotmail.com>
2019-06-28 12:22:59 +0800

    added watch buttons plus scroll bar

---
+++ Assets/Vuforia/Scripts/AutoResizeGridElements.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AutoResizeGridElements : MonoBehaviour
+{
+        
+    Canvas canvas;
+    GridLayoutGroup gridLayout;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = this.GetComponentInParent<Canvas>();
+        gridLayout = this.GetComponent<GridLayoutGroup>();
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cellX = canvas.pixelRect.width * 0.1916f;
+        float cellY = canvas.pixelRect.height * 0.82f / 3;
+        gridLayout.cellSize = new Vector2(cellX, cellY);
+         
+    }
+}
