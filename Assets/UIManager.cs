45acd8ade49bdb71032d7cb0a742bd22fea62956
Wong Jin Fu Shaun <sw_best@hotmail.com>
2019-06-29 17:31:50 +0800

    Revert "UI Scaled to andriod device"
    This reverts commit 52b39d647b583f01e1299a2fc31c6d774382fc01.

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -9,19 +9,14 @@ public class UIManager : MonoBehaviour
     public GameObject ModelsManager;
     public Sprite[] SelectedSprites;
     public Sprite[] NormalSprites;
-    public Sprite[] CardSprites;
     public Image[] SliderImages;
-    public Image InformationCard;
-    public GameObject InformationIcon;
-    public GameObject InformationPanel;
+
 
     // Start is called before the first frame update
     void Start()
     {
 
         DeselectAll();
-        ToggleInformationIcon(false);
-        InformationPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,30 +30,7 @@ public class UIManager : MonoBehaviour
         ModelsManager.SendMessage("SetModel",index);
         DeselectAll();
         SliderImages[index].sprite = SelectedSprites[index];
-        
-        if (index != 3)
-        {
-            InformationCard.sprite = CardSprites[index];
-            ToggleInformationIcon(true);
-
-
-        } else
-        {
-            ToggleInformationIcon(false);
-        }
-        
     }
-
-    public void ShowInformation()
-    {
-        InformationPanel.SetActive(true);
-    }
-
-    public void HideInformation()
-    {
-        InformationPanel.SetActive(false);
-    }
-
 
     void DeselectAll()
     {
@@ -66,10 +38,5 @@ public class UIManager : MonoBehaviour
         {
             SliderImages[i].sprite = NormalSprites[i];
         }
-        ToggleInformationIcon(false);   
-    }
-    void ToggleInformationIcon(bool state)
-    {
-        InformationIcon.SetActive(state);
     }
 }
